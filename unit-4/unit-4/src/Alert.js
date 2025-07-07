ee6461983c74b7c7713b771af29c37f26b73b5a0
Davidmitseruk <davidmitseruk@gmail.com>
2025-07-07 15:48:17 +0200

    ll

--- unit-4/unit-4/src/Alert.js
+++ unit-4/unit-4/src/Alert.js
@@ -1,28 +1,36 @@
-import { children } from "react"
+import React from "react"
+import css from './Alert.module.css'
 
-const alertStyle = {
-   color: 'white',
-   borderRadius: 4,
-   padding: '12px 18px',
-   margin: 10
-};
-
-const getColor = variant => {
-    switch(variant) {
-        case "info":
-            return "skyblue";
-        case 'succes':
-            return "green";
-        case 'warning':
-            return "orange";
-        case 'error':
-            return "red";
-        default:
-            throw new Error(`Prop: ${variant}`)
-    }
+export const Alert = ({children}) => {
+    return <p className={css.alert}>{children}</p>;
 }
 
 
-export const Alert = ({children, variant}) => {
-    return <p style={{...alertStyle, backgroundColor: getColor(variant)}}> { children }</p>
-}
+
+
+// const alertStyle = {
+//    color: 'white',
+//    borderRadius: 4,
+//    padding: '12px 18px',
+//    margin: 10
+// };
+
+// const getColor = variant => {
+//     switch(variant) {
+//         case "info":
+//             return "skyblue";
+//         case 'succes':
+//             return "green";
+//         case 'warning':
+//             return "orange";
+//         case 'error':
+//             return "red";
+//         default:
+//             throw new Error(`Prop: ${variant}`)
+//     }
+// }
+
+
+// export const Alert = ({children, variant}) => {
+//     return <p style={{...alertStyle, backgroundColor: getColor(variant)}}> { children }</p>
+// }
